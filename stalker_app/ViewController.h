bebafd0f9f0b875a6b73142de7b24963279e7178
David ZHANG <zigzhang@Davids-MacBook-Pro.local>
2013-10-06 13:08:59 -0700

    Initial Commit

---
+++ stalker_app/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  stalker_app
+//
+//  Created by David ZHANG on 10/6/13.
+//  Copyright (c) 2013 David ZHANG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ stalker_appTests/stalker_appTests.m
@@ -0,0 +1,34 @@
+//
+//  stalker_appTests.m
+//  stalker_appTests
+//
+//  Created by David ZHANG on 10/6/13.
+//  Copyright (c) 2013 David ZHANG. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface stalker_appTests : XCTestCase
+
+@end
+
+@implementation stalker_appTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
